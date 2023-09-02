0b75988504b9f274acb3f881b5e6c37b01f63af2
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-09-02 10:45:50 -0700

    FULL COMPLETE
    FULL COMPLETE

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 var h1 = document.querySelector('h1')
  var btn = document.querySelector('button')
+ var img = document.querySelector('img')
+var i =document.querySelector('i')
+img.addEventListener('dblclick',function(){
+
+    i.style.opacity=1
+    
+    setTimeout(function(){
+    i.style.opacity=0
+
+},1000)
+})
+
+
 
 var flag = 1
 
@@ -23,3 +36,4 @@ else{
 }
 
  })
+ 
